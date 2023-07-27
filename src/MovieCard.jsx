98b458cc6dcd2aeb589bce08c9bccec93d158015
Amar Kumbhar <amarakumbhar1234@gmail.com>
2023-07-27 16:32:59 +0530

    initial commit

---
+++ src/MovieCard.jsx
@@ -0,0 +1,26 @@
+import react from "react";
+
+function MovieCard({ movie }) {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+      <div>
+        <img
+          src={
+            movie.Poster !== ""
+              ? movie.Poster
+              : "http://vai.placeholder.com/400"
+          }
+        />
+      </div>
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
